3233e9a2e39ffd95c2b3913f763f5d377855d8b1
Pierre <pierre.balland@edu.ece.fr>
2019-10-27 23:44:41 +0100

    Bug températures réglé

--- js/script.js
+++ js/script.js
@@ -74,25 +74,23 @@ function threedays() {
  
       // On récupère l'information principal du lendemain
       const main = data.list[1].weather[0].main;
-      alert(main);
-       const description_meteo = data.list[1].weather[0].description;
-      alert(description_meteo);
+      const description_meteo = data.list[1].weather[0].description;
       
-     // const temp = data.list[0].temp[0].day;
-     // alert(temp);
+      const temp = (Math.round((data.list[1].temp.day) -273));
+      
       const icon = apiWeather.getHTMLElementFromIcon(data.list[1].weather[0].icon);
       // Modifier le DOM
       document.getElementById('today-forecast-main2').innerHTML = main;
       document.getElementById('today-forecast-more-info2').innerHTML = description_meteo;
       document.getElementById('icon-weather-container2').innerHTML = icon;
-      //document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+      document.getElementById('today-forecast-temp2').innerHTML = `${temp}°C`;
      
      //surlendemain
      const main2 = data.list[2].weather[0].main;
       
        const description_meteo2 = data.list[2].weather[0].description;
       
-      
+      const temp2 = (Math.round((data.list[2].temp.day) -273));
      // const temp = data.list[0].temp[0].day;
      // alert(temp);
       const icon2 = apiWeather.getHTMLElementFromIcon(data.list[2].weather[0].icon);
@@ -100,14 +98,14 @@ function threedays() {
       document.getElementById('today-forecast-main3').innerHTML = main2;
       document.getElementById('today-forecast-more-info3').innerHTML = description_meteo2;
       document.getElementById('icon-weather-container3').innerHTML = icon2;
-      //document.getElementById('today-forecast-temp3').innerHTML = `${temp}°C`;
+      document.getElementById('today-forecast-temp3').innerHTML = `${temp2}°C`;
 
       //toujours plus loin 
      const main3 = data.list[3].weather[0].main;
       
        const description_meteo3 = data.list[3].weather[0].description;
       
-      
+      const temp3 = (Math.round((data.list[3].temp.day) -273));
      // const temp = data.list[0].temp[0].day;
      // alert(temp);
       const icon3 = apiWeather.getHTMLElementFromIcon(data.list[3].weather[0].icon);
@@ -115,7 +113,7 @@ function threedays() {
       document.getElementById('today-forecast-main4').innerHTML = main3;
       document.getElementById('today-forecast-more-info4').innerHTML = description_meteo3;
       document.getElementById('icon-weather-container4').innerHTML = icon3;
-      //document.getElementById('today-forecast-temp3').innerHTML = `${temp}°C`;
+      document.getElementById('today-forecast-temp4').innerHTML = `${temp3}°C`;
  }) 
     .catch(function(error) {
       // Affiche une erreur
